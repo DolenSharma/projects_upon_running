2f24e66a799abf8486d2df0154cda96fdc0f3c80
Dolen Sharma <dolensharma12@gmail.com>
2023-05-02 06:53:56 -0400

    try

--- app/Filament/Resources/BdmpostResource.php
+++ app/Filament/Resources/BdmpostResource.php
@@ -44,7 +44,10 @@ class BdmpostResource extends Resource
     //----------------------------------------------------------------//
                 
                 TextInput::make('visa_required')
+<<<<<<< HEAD
                 ->label('Visa Required')
+=======
+>>>>>>> 902705a1 (all files loaded)
                 ->required()
                 ->datalist([
                     'GC EAD',
